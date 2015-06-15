cf240496fcb297b508f60ee59df713d703302254
sbarden <sbarden@inadco.com>
2015-06-15 14:40:08 -0500

    updated dbsetup.rb

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -7,5 +7,11 @@ node[:deploy].each do |app_name, deploy|
   )'"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
+
+  end
+    execute "mysql-insert-row" do
+    command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'INSERT INTO #{node[:phpapp][:dbtable]}(message) values('Thermonuclear War.');"
+    not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
+    action :run
   end
 end
