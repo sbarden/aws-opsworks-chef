0d59a9ee35bee1baf7c2d3f94a73d21d00d53924
sbarden <sbarden@inadco.com>
2015-06-15 14:51:25 -0500

    insert

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -7,11 +7,9 @@ node[:deploy].each do |app_name, deploy|
   )'"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
-
-  end
-    execute "mysql-insert-row" do
+end
+  execute "mysql-insert-row" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'INSERT INTO #{node[:phpapp][:dbtable]}(message) values('Thermonuclear War.');"
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
-  end
 end
--- phpapp/recipes/insert.rb
+++ phpapp/recipes/insert.rb
@@ -4,4 +4,3 @@ node[:deploy].each do |app_name, deploy|
     not_if "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'SHOW TABLES' | grep #{node[:phpapp][:dbtable]}"
     action :run
   end
-end
