d0fa186ab6cdfe822583fe2bb23a4946d0d4c3e5
sbarden <sbarden@inadco.com>
2015-06-15 14:31:02 -0500

    updated dbsetup.rb

--- phpapp/recipes/dbsetup.rb
+++ phpapp/recipes/dbsetup.rb
@@ -2,7 +2,6 @@ node[:deploy].each do |app_name, deploy|
   execute "mysql-create-table" do
     command "/usr/bin/mysql -u#{deploy[:database][:username]} -p#{deploy[:database][:password]} #{deploy[:database][:database]} -e'CREATE TABLE #{node[:phpapp][:dbtable]}(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    author VARCHAR(63) NOT NULL,
     message TEXT,
     PRIMARY KEY (id)
   )'"
